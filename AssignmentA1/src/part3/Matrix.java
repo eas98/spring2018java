aefd5f6fb196d5827e706da53ec60ae278a14029
samaniehsan <ehsan.samani-social@outlook.com>
2018-03-17 11:56:31 -0500

    matrix multipication completed

--- AssignmentA1/src/part3/Matrix.java
+++ AssignmentA1/src/part3/Matrix.java
@@ -1,5 +1,7 @@
 package part3;
 
+import java.util.Random; 
+
 public class Matrix {
 	Integer m_n; 
 	Integer m_m;
@@ -10,7 +12,16 @@ public class Matrix {
 		m_data = new Integer [n][m]; 
 	}
 	
-	public Matrix Add(Matrix left, Matrix right) throws MatrixDimensionException {
+	public void generate() {
+		 Random rand = new Random();
+		 for(int i=0;i<m_n;i++) {
+			 for(int j=0;j<m_m;j++) {
+				 Integer data = rand.nextInt(100);
+				 m_data[i][j] = data;
+			 } 
+		 }
+	}
+	public Matrix add(Matrix left, Matrix right) throws MatrixDimensionException {
 		boolean equalDemensions = 
 				left.m_m == right.m_m && 
 				left.m_n == right.m_n;
@@ -18,6 +29,28 @@ public class Matrix {
 			throw new MatrixDimensionException("Matrices being addded should have equal dimensions");
 		}
 		Matrix result = new Matrix(left.m_n, left.m_m);
+		for(int i=0;i<left.m_n;i++) {
+			for(int j=0;j<left.m_m;j++) {
+				result.m_data[i][j] = left.m_data[i][j] + right.m_data [i][j];
+			}
+		}
+		return result;
+	}
+	public Matrix multiply(Matrix left, Matrix right) throws MatrixDimensionException {
+		boolean multipliable = left.m_m == right.m_n;
+		if(!multipliable) {
+			throw new MatrixDimensionException("Number of column in left Matrix has to equal number of rows in right Matrix");
+		}
+		Matrix result = new Matrix(left.m_n, right.m_m);
+		for(int i=0;i<left.m_n;i++) {
+			int cellValue = 0;
+			for(int j=0;j<right.m_m;j++) {
+				for(int ii=0;ii<left.m_n;ii++) {
+					cellValue += left.m_data[i][ii] * right.m_data[j][ii];	
+				}
+				result.m_data[i][j] = cellValue;
+			}
+		}
 		return result;
 	}
 }
