1a2dfba4733a84c73f427112b1e27226604cd3b1
samaniehsan <ehsan.samani-social@outlook.com>
2018-03-21 00:29:52 -0500

    part3 string search

--- AssignmentA1/src/part2/HayStack.java
+++ AssignmentA1/src/part2/HayStack.java
@@ -10,14 +10,31 @@ public class HayStack implements SubstringFrequencySearcher{
 	public int findFrequency(String subString) {
 		if(subString == null && m_searchSpace == null)
 			return 1;
+		if(m_searchSpace == null)
+			return 0;
+		if(subString == null)
+			return 0;
 		if(subString.isEmpty() && m_searchSpace.isEmpty())
 			return 1;
+		if(!m_searchSpace.isEmpty())
+			return m_searchSpace.length();
 		
 		if(m_searchSpace != null) {
 			int nFrequency = 0;
-			for(int i =0;i<m_searchSpace.length();i++) {
-				m_searchSpace
+			int nMatchedIndex = 0;
+			for(int i=0;i<m_searchSpace.length();i++) {
+				char ch = m_searchSpace.charAt(i);
+				if(ch == subString.charAt(nMatchedIndex)) {
+					nMatchedIndex++;
+					if(subString.length() == nMatchedIndex) {
+						nFrequency++;
+						nMatchedIndex= 0;
+					}
+				} else {
+					nMatchedIndex = 0;
+				}
 			}
+			return nFrequency;
 		}
 		return 0;
 	}
--- AssignmentA1/src/part2/Part2.java
+++ AssignmentA1/src/part2/Part2.java
@@ -1,10 +1,28 @@
 package part2;
 
+import java.util.Scanner;
+
 public class Part2 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("part2");
+		String searchSpace = null;
+		String subString = null;
+		if(args.length == 2) {
+			searchSpace =args[0];
+			subString =args[1];
+		}
+		
+		while(searchSpace !=null && subString != null) {
+			System.out.print("search space string:");
+			Scanner scanner = new Scanner(System.in);
+		    String largeString = scanner.nextLine();
+		    System.out.print("search string:");
+		}
+			
+		SubstringFrequencySearcher searcher = new HayStack(searchSpace);
+		int nFrequency = searcher.findFrequency(subString);
+		System.out.println("Found %d instances",nFrequency);
 	}
 
 }
