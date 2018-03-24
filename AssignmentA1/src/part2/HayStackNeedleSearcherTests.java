8a0185ae034b233b0c5d3dce5d68e20025bebdb2
samaniehsan <ehsan.samani-social@outlook.com>
2018-03-23 21:05:05 -0500

    adding haystack default tests

--- AssignmentA1/src/part2/HayStackNeedleSearcherTests.java
+++ AssignmentA1/src/part2/HayStackNeedleSearcherTests.java
@@ -49,4 +49,26 @@ class HayStackNeedleSearcherTests {
 		int nFrequency = searcher.findFrequency("A");
 		assertEquals(nFrequency,2);
 	}
+	@Test
+	void testEarthQuakeWithSearchSpace() {
+		String searchSpace = "The western American city of San Francisco, California suffered a huge earthquake on April 18th, 1906."+
+		"More than three thousand people are known to have died. The true number of dead will never be known. Two"+
+		"hundred fifty thousand people lost their homes. Just a few hours after the terrible earthquake, a magazine named"+
+		"Collier's sent a telegraph message to the famous American writer Jack London. They asked Mr. London to go to"+
+		"San Francisco and report about what he saw.";
+		HayStackNeedleSearcher searcher = new HayStackNeedleSearcher(searchSpace);
+		int nFrequency = searcher.findFrequency("earthquake");
+		assertEquals(nFrequency,2);
+	}
+	@Test
+	void testSanFranciscoWithSearchSpace() {
+		String searchSpace = "The western American city of San Francisco, California suffered a huge earthquake on April 18th, 1906."+
+		"More than three thousand people are known to have died. The true number of dead will never be known. Two"+
+		"hundred fifty thousand people lost their homes. Just a few hours after the terrible earthquake, a magazine named"+
+		"Collier's sent a telegraph message to the famous American writer Jack London. They asked Mr. London to go to"+
+		"San Francisco and report about what he saw.";
+		HayStackNeedleSearcher searcher = new HayStackNeedleSearcher(searchSpace);
+		int nFrequency = searcher.findFrequency("San Francisco");
+		assertEquals(nFrequency,2);
+	}
 }
