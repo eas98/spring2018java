af65e285dec8e4af703c9516d3962d0d0bae4b4e
samaniehsan <ehsan.samani-social@outlook.com>
2018-04-21 18:52:30 -0500

    swing better spacing

--- assignmenta2/CalendarApp/src/calendarapp/CalendarApp.java
+++ assignmenta2/CalendarApp/src/calendarapp/CalendarApp.java
@@ -1,9 +1,11 @@
 package calendarapp;
 
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.text.SimpleDateFormat;
@@ -15,21 +17,27 @@ import org.jdesktop.swingx.calendar.DateSelectionModel.SelectionMode;
 
 
 public class CalendarApp {
-    private static void addControls(JFrame frame) {
-        JPanel panelMain = new JPanel(new GridBagLayout());
-        
-        GridBagConstraints c1 = new GridBagConstraints();
+    
+	private static Component addTopPanel() {
+		
+		GridBagConstraints c1 = new GridBagConstraints();
         c1.fill = GridBagConstraints.HORIZONTAL;
-        //c.anchor = GridBagConstraints.CENTER;
-        c1.gridx = 1;
+        c1.anchor = GridBagConstraints.LINE_START;
+        c1.gridx = 2;
+        c1.weighty = .1;
         
         JPanel subPanelTop = new JPanel(new GridBagLayout());
+        subPanelTop.setLayout(new GridLayout(1, 1, 0, 0));
         
         JLabel label = new JLabel("Save your Appointments");
         
         subPanelTop.add(label,c1);
-    
-    	JPanel subPanelMiddle = new JPanel(new GridBagLayout());
+        return subPanelTop;
+	}
+	private static Component addMiddlePanel() {
+		GridBagConstraints c = new GridBagConstraints();
+		JPanel subPanelMiddle = new JPanel(new GridBagLayout());
+		subPanelMiddle.setLayout(new GridLayout(1, 3, 3, 3));
     	
     	JXMonthView  monthView = new JXMonthView();
         monthView.setTraversable(true);
@@ -41,19 +49,24 @@ public class CalendarApp {
                 }
             }
         });
-        monthView.setSelectionMode(SelectionMode.SINGLE_INTERVAL_SELECTION);    	
-        //c.anchor = GridBagConstraints.WEST;
-        GridBagConstraints c = new GridBagConstraints();
-        c.fill = GridBagConstraints.HORIZONTAL;
+        monthView.setSelectionMode(SelectionMode.SINGLE_INTERVAL_SELECTION);
+        monthView.setBorder(BorderFactory.createCompoundBorder(
+        		monthView.getBorder(), 
+    	        BorderFactory.createEmptyBorder(10, 5, 10, 5)));
         
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.WEST;
         
         
     	JTextArea textField = new JTextArea(12,15);
     	textField.setText("Enter\nYour Appointment Note");
-    	//c.anchor = GridBagConstraints.EAST;
+    	textField.setBorder(BorderFactory.createCompoundBorder(
+    			textField.getBorder(), 
+    	        BorderFactory.createEmptyBorder(10, 5, 10, 5)));
+    	c.anchor = GridBagConstraints.EAST;
     	c.fill = GridBagConstraints.HORIZONTAL;
     	
-        c.weightx = 0.4;
+        c.weightx = 0.3;
         c.gridy = 0;
     	c.gridx = 1;
     	
@@ -61,22 +74,23 @@ public class CalendarApp {
     	
         c.weightx = .4;
         c.gridy = 0;
-        c.gridx = 4;
+        c.gridx = 6;
     	
     	subPanelMiddle.add(textField,c);
     	
-    	GridBagConstraints c2 = new GridBagConstraints();
-    	c2.fill = GridBagConstraints.VERTICAL;
-    	
-    	
-    	JPanel subPanelBottom = new JPanel(new GridBagLayout());
-    	
+        return subPanelMiddle;
+	}
+	private static Component addBottomPanel() {
+		JPanel subPanelBottom = new JPanel(new GridBagLayout());
+		subPanelBottom.setLayout(new GridLayout(1, 3, 2, 2));
+		
+		GridBagConstraints c = new GridBagConstraints();
         
     	c.anchor = GridBagConstraints.WEST;
     	c.fill = GridBagConstraints.HORIZONTAL;
     	c.weighty=0.3;
     	c.weightx=0.4;
-    	c.gridy = 2;
+    	c.gridy = 4;
     	c.gridx = 1;
     	
     	JButton okButton = new JButton("Ok");
@@ -86,23 +100,37 @@ public class CalendarApp {
     	c.anchor = GridBagConstraints.EAST;
     	c.weighty=.3;
     	c.weightx=.4;
-    	c.gridy = 2;
-    	c.gridx = 4;
+    	c.gridy = 4;
+    	c.gridx = 5;
     	
     	JButton cancelButton = new JButton("Cancel");
+    	
+    	
     	subPanelBottom.add(cancelButton,c);
     	
+    	return subPanelBottom;
+	}
+	private static void addControls(JFrame frame) {
+        JPanel panelMain = new JPanel(new GridBagLayout());
+        
+        Component subPanelTop = addTopPanel();
+        Component subPanelMiddle = addMiddlePanel();
+        Component subPanelBottom = addBottomPanel();
+            	
     	GridBagConstraints cmain = new GridBagConstraints();
-    	cmain.fill = GridBagConstraints.VERTICAL;
-    	cmain.anchor = GridBagConstraints.NORTH;
-    	cmain.gridy=1;
+    	cmain.fill = GridBagConstraints.NORTH;
+    	cmain.gridy=0;
+    	cmain.weighty=.1;
     	panelMain.add(subPanelTop,cmain);
-    	cmain.anchor = GridBagConstraints.CENTER;
-    	cmain.gridy=2;
+    	cmain.gridy=1;
+    	cmain.weighty=.6;
+    	cmain.fill = GridBagConstraints.CENTER;
     	panelMain.add(subPanelMiddle,cmain);
-    	cmain.anchor = GridBagConstraints.SOUTH;
-    	cmain.gridy=3;
+    	cmain.gridy=2;
+    	cmain.weighty=.1;
+    	cmain.fill = GridBagConstraints.SOUTH;
     	panelMain.add(subPanelBottom,cmain);
+    	
     	
     	frame.getContentPane().add(panelMain);
     }
@@ -113,7 +141,7 @@ public class CalendarApp {
         
         //minimum resolution on a modern monitor 1024*.768
         //This size seems reasonable.
-        Dimension d = new Dimension(400,400);
+        Dimension d = new Dimension(600,400);
         Container c = frame.getContentPane();
         c.setPreferredSize(d);
         
